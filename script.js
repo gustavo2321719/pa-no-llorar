8ce3095b2a31c4e47a75173a8687ebcd3726bb5f
gustavo2321719 <gustavo2321719@gmail.com>
2024-05-06 19:47:33 -0500

    9876
    sdafg

--- script.js
+++
@@ -1,9 +0,0 @@
-// Obtener los componentes de la fecha
-var fechaComponents = fecha.split("-");
-// Reorganizar los componentes en el formato deseado
-var fechaFormateada = fechaComponents[2] + "/" + fechaComponents[1] + "/" + fechaComponents[0];
-// Insertar la fecha formateada en la columna "Fecha de DJ"
-datos[3] = fechaFormateada;
-
-// Agregar "'" al inicio del contenido de la celda de DNI
-datos[4] = "'" + datos[4];
